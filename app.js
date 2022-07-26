bdd38d4182c97b8aba551eef5756b1fb63176fc9
Soham De <sohamsdays@gmail.com>
2022-07-26 14:52:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks.js");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+//middleware
+app.use(express.static("./public"));
+app.use(express.json());
+
+//routes
+// app.get("/hello", (req, res) => {
+//   res.send("Task Manager API");
+// });
+
+app.use("/api/v1/tasks", tasks);
+
+const port = 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`This is port${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ controllers/tasks.js
@@ -0,0 +1,71 @@
+const Task = require("../models/Task.js");
+
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+};
+
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+    console.log(req.body, task);
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+};
+
+const getOneTask = async (req, res) => {
+  // res.json({ idesadsad: req.params.id });
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id:${taskID}` });
+    }
+    res.json({ task });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
+};
+
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: "No task with the id" });
+    }
+    res.status(201).json({ task });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
+};
+
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).json({ msg: "No task with the id" });
+    }
+    res.status(200).json({ task });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
+};
+
+module.exports = {
+  getAllTasks,
+  createTask,
+  getOneTask,
+  updateTask,
+  deleteTask,
+};
